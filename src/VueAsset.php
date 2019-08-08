f0b611b5624ac2f24be5d1c13b281e9ecf62a1cf
Jomon Johnson <jomon.entero@gmail.com>
2019-08-08 17:33:06 +0530

    init

--- src/VueAsset.php
+++ src/VueAsset.php
@@ -8,12 +8,10 @@ use yii\web\View;
 class VueAsset extends AssetBundle
 {
     public $sourcePath = '@npm/vue/dist';
-//    public $js = [
-//        'vue.esm.browser.min.js',
-//    ];
     public $js = [
-        'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js',
+        YII_ENV_DEV ? 'vue.js' : 'vue.min.js',
     ];
+
     public $jsOptions = [
         'position' => View::POS_HEAD,
     ];
